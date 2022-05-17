bb51a18c14da5ed07793b2259dc73794c576c4d6
Huynh, Tu | RASIA <tu.huynh@rakuten.com>
2022-05-17 22:05:18 +0800

    Whee.

--- server/main.js
+++ server/main.js
@@ -98,7 +98,6 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const e = require("express")
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
